7704fa6d0025c3e1d21771f2cd9b977625356913
Mykhailo Petrenko <mykhailo@opt-net.eu>
2024-11-11 17:05:56 +0100

    Test the output

--- app/jenkins/scripts/deliver.sh
+++ app/jenkins/scripts/deliver.sh
@@ -22,5 +22,9 @@ set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
+
+pwd
+id
+
 set -x
 java -jar target/${NAME}-${VERSION}.jar
